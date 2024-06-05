d7eaf6474e9d8469a1a27b77c479d77162ee61e0
Astronedo <astronedolacambra@gmail.com>
2024-06-05 15:01:36 -0400

    Facebook first commit!

---
+++ Facebook/script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Example of interactivity: updating user info
+  const userInfo = document.querySelector(".user-info");
+  userInfo.addEventListener("click", function () {
+    alert("User info clicked!");
+  });
+});
